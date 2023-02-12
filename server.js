aeefc74f9db1cfb56497bdd974efd31558fdeb5a
Kristina Alabastro <kristinaa7@live.com>
2023-02-12 14:25:05 -0800

    added get request to join with index.html

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
+//Dependencies needed for the code to run
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
-
+const notes = require('./db/db.json');
 const app = express()
-const PORT = process.env.PORT 
-|| 3001; 
+const PORT = process.env.PORT || 3001; 
 
-const testdb = require('./db/db.json');
+//Middleware to set up the Express app to handle data parsing
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
-app.get("/", function(req,res){
-    res.sendFile(path.join());
-})
+//get request for notes
+app.get('/', (req,res){
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
+app.get('/api/notes', (req,res) => res.json());
+
+
+// post request
 app.post("/", function(req,res){
 })
 
+//delete request
 app.delete("/", function(req,res){
     res.sendFile(path.join());
 })
