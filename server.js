77e5cc5a40decb40b90f55cc07c3df5de169ca0b
Kristina Alabastro <kristinaa7@live.com>
2023-02-15 19:07:11 -0800

    added correct path and new app.get

--- server.js
+++ server.js
@@ -11,13 +11,17 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
 
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/..public/notes.html'))
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 //GET request for notes
 app.get('/api/notes', (req, res) => {
@@ -64,7 +68,6 @@ app.delete ("/api/notes/:id", (req,res) => {
     let parsedNotes = JSON.parse(data);
     const notesId = parsedNotes.filter((note)=> note.id !==req.params.id)
 })
-
     fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(parsedNotes))
 });
 
