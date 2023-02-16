1163a57ef32e17a9246d1459d6b1d539c3a3ff92
Kristina Alabastro <kristinaa7@live.com>
2023-02-15 20:01:06 -0800

    added get * path to index

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const express = require('express')
 const path = require('path')
 const fs = require('fs')
 const notes = require('./db/db.json');
-const app = express()
+
+const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 //Middleware to set up the Express app to handle data parsing
 app.use(express.json());
-app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) => {
@@ -20,6 +23,10 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 //GET request for notes
 app.get('/api/notes', (req, res) => {
